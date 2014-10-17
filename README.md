1356870350cb50644b094c3f197ac5351e6db2b5
Joe Woods <woodsja@vcu.edu>
2014-10-17 19:22:33 -0400

    make

---
+++ README.md
@@ -0,0 +1,11 @@
+habitrpg-boss-button
+====================
+
+Chrome extension to create a "boss button" to remove game-looking elements from HabitRPG, permitting its safe use at work or in class. 
+
+See:  [HabitRPG](https://github.com/HabitRPG/habitrpg)
+
+Image of the button in action: 
+![image goes here](http://i.imgur.com/QOoqqhh.png)
+
+Simple, huh?
---
+++ src/inject/inject.js
@@ -0,0 +1,39 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		//console.log("Hello. This message was sent from scripts/inject.js");
+		// ----------------------------------------------------------
+
+/*function bossbutton(){
+	if ($("#undefined-sticky-wrapper").is(":visible") == true){
+		$("undefined-sticky-wrapper").hide();
+		$(".toolbar-wallet").hide();
+		$(".rewards").hide();
+	} else {
+		$("undefined-sticky-wrapper").show();
+		$(".toolbar-wallet").show();
+		$(".rewards").show();
+	}
+}*/
+
+
+function arachnophobia(){
+	var list = ["Base","White","Desert","Red","Shade","Skeleton","Zombie","CottonCandyPink","CottonCandyBlue","Golden"];
+
+	for (var i=0, item; item = list[i]; i++) {
+		//item
+  	$(".Pet-Spider-"+item).hide();
+		$(".Mount_Head_Spider-"+item).hide();
+		$(".Mount_Body_Spider-"+item).hide();
+	}
+}
+
+arachnophobia();
+
+	}
+}, 1000);
+});
